54617f9a674e2a86cd4a1b5fda30c1ec03b1b800
DESKTOP-1SHLQ4P\milos <stojanovic.milos14182@gmail.com>
2017-05-10 15:39:29 +0200

    -socket-i

--- src/main/java/com/milos/neo4j/intrestinggeography/SpringSecurity.java
+++ src/main/java/com/milos/neo4j/intrestinggeography/SpringSecurity.java
@@ -27,7 +27,7 @@ public class SpringSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
+        http.headers().frameOptions().sameOrigin();
         http.authorizeRequests()
                 .antMatchers("/profile/**").access("hasRole('ROLE_USER')")
                 .antMatchers("/play/**").access("hasRole('ROLE_USER')")
